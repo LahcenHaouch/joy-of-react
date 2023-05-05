3f42a7fe0bca5407cd24950908ec45a1a2cd491b
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-05-05 23:00:18 +0200

    Display list of previous guesses

--- word-clone/src/components/Game/Game.js
+++ word-clone/src/components/Game/Game.js
@@ -3,31 +3,37 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
+import PreviousGuesses from './PreviousGuesses';
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  const [guess, setGuess] = React.useState('');
+  const [guesses, setGuesses] = React.useState([]);
+  const [currentGuess, setCurrentGuess] = React.useState('');
 
   const handleGuessSubmit = (event) => {
     event.preventDefault();
-    console.log(guess);
-    setGuess('');
+    setGuesses(prevGuesses => prevGuesses.concat({ id: crypto.randomUUID(), value: currentGuess }))
+    setCurrentGuess('');
   }
 
   return (
-    <form on onSubmit={handleGuessSubmit} className="guess-input-wrapper">
-      <label htmlFor="guess-input">Enter guess:</label>
-      <input
-        id="guess-input"
-        type="text"
-        pattern="\w{5,5}"
-        value={guess}
-        onChange={event => setGuess(event.target.value.trim().toUpperCase())}
-      />
-    </form>
+    <div>
+      <form on onSubmit={handleGuessSubmit} className="guess-input-wrapper">
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          pattern="\w{5,5}"
+          value={currentGuess}
+          onChange={event => setCurrentGuess(event.target.value.trim().toUpperCase())}
+        />
+      </form>
+      <PreviousGuesses guesses={guesses} />
+    </div>
   )
 }
 
